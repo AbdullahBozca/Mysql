ba84c67c78070b5155a61c5d5c19599664b8d84e
Abdullah Bozca <abdullahbozca@gmail.com>
2022-01-06 00:22:34 +0300

    06/01/2022

--- day06_grupBy.sql
+++ day06_grupBy.sql
@@ -28,3 +28,59 @@
     INSERT INTO manav VALUES( 'Veli', 'Elma', 3);
     INSERT INTO manav VALUES( 'Ayse', 'Uzum', 4);
     INSERT INTO manav VALUES( 'Ali', null, 2);
+    
+/* -----------------------------------------------------------------------------
+  ORNEK1: kisi ismine göre satılan toplam meyve miktarlarını gösteren sorguyu 
+  yazınız.
+------------------------------------------------------------------------------*/  
+SELECT 
+    isim, SUM(urun_miktari) toplam
+FROM
+    manav
+GROUP BY isim;
+
+/* ----------------------------------------------------------------------------
+  ORNEK2: satılan meyve türüne (urun_adi) göre urun alan kişi sayısını gösteren
+  sorguyu yazınız. NULL olarak girilen meyveyi listelemesin.
+-----------------------------------------------------------------------------*/ 
+SELECT 
+    urun_adi, COUNT(isim)
+FROM
+    manav
+WHERE
+    urun_adi IS NOT NULL
+GROUP BY urun_adi;
+
+/* ----------------------------------------------------------------------------
+  ORNEK3: satılan meyve türüne (urun_adi) göre satılan (urun_miktari )MIN ve 
+  MAX urun miktarlarini, MAX urun miktarina göre sıralayarak listeyen sorguyu 
+  yazınız.
+-----------------------------------------------------------------------------*/ 
+SELECT 
+    urun_adi, MIN(urun_miktari) min, MAX(urun_miktari) max
+FROM
+    manav
+WHERE
+    urun_adi IS NOT NULL
+GROUP BY urun_adi
+ORDER BY max;
+
+/* ----------------------------------------------------------------------------
+  ORNEK4: kisi ismine ve urun adına göre satılan ürünlerin toplamını 
+  gruplandıran ve isime göre ters sırasıda listeyen sorguyu yazınız.
+ -----------------------------------------------------------------------------*/ 
+ 
+ select isim, urun_adi, sum(urun_miktari) from manav where urun_adi is not null group by isim,urun_adi order by isim desc;
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
